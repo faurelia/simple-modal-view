edea1b205065e4834367377f14ae605870d9830a
Ernesto Navarro <ernesto.navarro@gmail.com>
2017-10-21 16:37:52 +0200

    Fixed problem that prevent click works in other elements

--- modal.js
+++ modal.js
@@ -8,12 +8,11 @@ document.addEventListener('click', function (e) {
     e = e || window.event;
     var target = e.target || e.srcElement;
 
-    e.preventDefault();
-
     if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal') {
         if (target.hasAttribute('data-target')) {
             var m_ID = target.getAttribute('data-target');
             document.getElementById(m_ID).classList.add('open');
+            e.preventDefault();
         }
     }
 
@@ -21,5 +20,6 @@ document.addEventListener('click', function (e) {
     if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal')) {
         var modal = document.querySelector('[class="modal open"]');
         modal.classList.remove('open');
+        e.preventDefault();
     }
 }, false);
